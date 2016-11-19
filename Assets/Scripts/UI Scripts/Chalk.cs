6ee41ada973c51c4d697f1f41bfc353bf99230e2
Pavel Vychuzhanin <crisp.coad@gmail.com>
2016-11-19 18:10:09 +0300

    Add offset checking for chalk

--- Assets/Scripts/UI Scripts/Chalk.cs
+++ Assets/Scripts/UI Scripts/Chalk.cs
@@ -3,8 +3,24 @@ using System.Collections;
 
 public class Chalk : MonoBehaviour {
 
+    private Vector3 defaultPos;
+    private float delta = 0.0f;
+    public float MAX_OFFSET = -70;
+
+    public void Start()
+    {
+        defaultPos = transform.position;
+    }
+
     public void OnDrag()
     {
-        transform.position = new Vector3(Input.mousePosition.x, transform.position.y, transform.position.z);
+        delta = Input.mousePosition.x - defaultPos.x;
+        if (delta < 0 && delta > MAX_OFFSET)
+        {
+            transform.position = new Vector3(Input.mousePosition.x, transform.position.y, transform.position.z);
+        } else if (delta <= MAX_OFFSET)
+        {
+            //enable draw mode
+        }
     }
 }
